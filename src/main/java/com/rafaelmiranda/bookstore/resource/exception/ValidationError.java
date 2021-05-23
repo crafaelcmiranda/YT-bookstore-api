4158f7fffba67f0c773876e5ae1cef39d3b9e343
crafael.cmiranda@gmail.com <crafael.cmiranda@gmail.com>
2021-05-23 10:42:35 -0300

    Configuração versão java 11 para heroku

--- src/main/java/com/rafaelmiranda/bookstore/resource/exception/ValidationError.java
+++ src/main/java/com/rafaelmiranda/bookstore/resource/exception/ValidationError.java
@@ -3,7 +3,6 @@ package com.rafaelmiranda.bookstore.resource.exception;
 import java.util.ArrayList;
 import java.util.List;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
